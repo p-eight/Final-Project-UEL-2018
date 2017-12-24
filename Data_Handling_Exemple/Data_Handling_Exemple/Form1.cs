d41cea23aac1baf6325c3d6e0ddcec92039c269c
Pedro Bonifacio <pedrobonifa@gmail.com>
2017-12-24 10:50:56 -0200

    Data Handler

---
+++ Data_Handling_Exemple/Data_Handling_Exemple/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Timers;
+using System.Threading;
+
+namespace Sistema_Supervisorio
+{
+    public partial class Form1 : Form
+    {
+        SerialPort PortaSerial = new SerialPort();
+        System.Timers.Timer general_timer = new System.Timers.Timer(1000);
+        public Form1()
+        {
+            InitializeComponent();
+            general_timer.Elapsed += General_timer_Handler;
+            general_timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Buscar_Porta_Serial();
+        }
+
+        public delegate void Delegate_void_NoPam();
+
+        public void Buscar_Porta_Serial()
+        {
+            try
+            {
+                /* Testa se a porta serial está aberta 
+                 * Se estiver, não é necessário atualizar a lista
+                   de portas seriais
+                 */
+                if (!PortaSerial.IsOpen)
+                {
+                    /* Procura as COMs disponíveis no computador */
+                    foreach (string str in SerialPort.GetPortNames())
+                    {
+                        /* Testa se a COM str já não está adicionada
+                         * na lista
+                         *  */
+                        if (comboBox1.FindString(str) == -1)
+                        {
+                            /* Adiciona a COM str na lista */
+                            comboBox1.Items.Add(str);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void General_timer_Handler(object sender, ElapsedEventArgs e)
+        {
+            if (!PortaSerial.IsOpen)
+            {
+                Invoke(new Delegate_void_NoPam(Buscar_Porta_Serial));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* switch para determinar ação a ser tomada */
+            switch (button1.Text)
+            {
+                case "Abrir Porta":
+                    if (PortaSerial.IsOpen)
+                    {
+                        PortaSerial.Close();
+                    }
+                    if ((comboBox1.Text == "") || (comboBox2.Text == ""))
+                    {
+                        MessageBox.Show("Os campos de configuração não podem ser deixados em branco");
+                        break;
+                    }
+                    try
+                    {
+                        /* Configuração da porta serial*/
+                        PortaSerial.PortName = comboBox1.Text;
+                        PortaSerial.BaudRate = int.Parse(comboBox2.Text);
+                        PortaSerial.Parity = Parity.None;
+                        PortaSerial.DataBits = 8;
+                        PortaSerial.StopBits = StopBits.One;
+                        PortaSerial.DataReceived += new SerialDataReceivedEventHandler(PortaSerial_DataReceivedHandler);
+
+                        PortaSerial.Open();
+
+                        button1.Text = "Fechar Porta";
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Erro ao abrir porta serial.");
+                    }
+                    break;
+
+                case "Fechar Porta":
+                    try
+                    {
+                        PortaSerial.Close();
+                        button1.Text = "Abrir Porta";
+                    }
+                    catch { }
+                    break;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /* As mensagens só podem ser enviadas se a porta serial estiver aberta
+             * e se o campo de dados não estiver vazio            
+             */
+            if (PortaSerial.IsOpen && richTextBox2.Text != "")
+            {
+                /* Os dados inseridos na caixa de texto
+                 * serão enviados em ASCII
+                 */
+                PortaSerial.Write(richTextBox2.Text);
+                richTextBox1.AppendText("TX: " + richTextBox2.Text + "\r\n");
+                richTextBox2.Clear();
+            }
+        }
+
+        private void PortaSerial_DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort Serial = (SerialPort)sender;
+            Thread.Sleep(20);
+            int bytes = Serial.BytesToRead;
+            byte[] recebido = new byte[bytes];
+            Serial.Read(recebido, 0, bytes);
+            Tratar_Mensagem(recebido);
+        }
+        public delegate void Delegate_void_String(RichTextBox textbox, string text);
+        public void Append_Text(RichTextBox textbox, string text)
+        {
+            textbox.AppendText(text);
+            textbox.ScrollToCaret();
+        }
+
+        private void Tratar_Mensagem(byte[] mensagem)
+        {
+            /*
+             * Cria uma string contendo a data e hora atual a partir do relógio interno do computador.
+             * O formato da data será do tipo: [dd/mm/aa - hh:mm:ss]
+             */
+            string timestamp = string.Format("[{0}/{1}/{2} - {3}:{4}:{5}]", DateTime.Now.ToString("dd"), DateTime.Now.ToString("MM"),
+                DateTime.Now.ToString("yy"), DateTime.Now.ToString("HH"), DateTime.Now.ToString("mm"), DateTime.Now.ToString("ss"));
+            if (mensagem == null)
+            {
+                BeginInvoke(new Delegate_void_String(Append_Text), new object[] { richTextBox1,
+                    string.Format("{0} - A mensagem recebida está vazia. \r\n", timestamp) });
+                return;
+            }
+
+            /*
+             * Agora é realizado o teste se a mensagem é válida, ou seja, se tem os valores esperados como
+             * início e fim de mensagem.
+             * Início de mensagem == 0x0A
+             * Fim de mensagem == 0xA0.
+             */
+            if ((mensagem[0] != 0x0A) || (mensagem[mensagem.Length - 1] != 0xA0))
+            {
+                /*
+                 * Se a mensagem for inválida, apenas aparecerá o alerta na tela e finaliza o método.
+                 */ 
+                BeginInvoke(new Delegate_void_String(Append_Text), new object[] { richTextBox1,
+                    string.Format("{0} - A mensagem recebida é inválida. \r\n", timestamp) });
+                return;
+            }
+            else
+            {
+                /*
+                 * Se a mensagem for válida, também é escrita a mensagem na tela.
+                 */ 
+                BeginInvoke(new Delegate_void_String(Append_Text), new object[] { richTextBox1,
+                    string.Format("{0} - A mensagem recebida é válida. \r\n Mensagem recebida: {1}.\r\n", timestamp, BitConverter.ToString(mensagem).Replace('-',' '))});
+                return;
+            }
+        }
+    }
+
+}
---
+++ TCP_Server_Example/TCP_Server_Example/Form1 (1).cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_Server_Example
+{
+
+    public partial class Form1 : Form
+    {
+        string IP4_Endereco;
+        string IP4_Porta;
+        static TcpListener servidorTCP = null;
+        static TcpClient clienteTCP = null;
+        static NetworkStream streamTCP = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Encontrar_IP();
+        }
+
+        public void Encontrar_IP()
+        {
+            try
+            {
+                /*
+                 * Este loop irá procurar por endereços DNS e determinar o IP
+                 * da máquina utilizada
+                 */
+                foreach (IPAddress IPA in Dns.GetHostAddresses(Dns.GetHostName()))
+                {
+                    if (IPA.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        IP4_Endereco = IPA.ToString();
+                        break;
+                    }
+                }
+                textBox1.Text = IP4_Endereco;
+                /*
+                 * Será utilizado como porta padrão a porta 5001
+                 */
+                textBox2.Text = "5001";
+            }
+            catch { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /* Neste switch é tomada a decisão de abrir ou fechar
+                 o servidor conforme o texto do botão.*/
+                switch (button1.Text)
+                {
+                    case "Abrir Servidor":
+                        /* Testa se a caixas de texto não estão vazias. Se estiverem
+                         * não é possível abrir o servidor. */
+                        if((textBox1.Text == "") && (textBox2.Text == ""))
+                        {
+                            MessageBox.Show("O servidor TCP/IP não pode ser iniciado com parâmetros em branco.", "Atenção");
+                            return;
+                        }
+                        /* Atribui uma nova instância TcpListener na variável servidorTCP.
+                         * Converte os valores das caixas de texto para formato apropriado
+                         * para o servidor. */
+                        servidorTCP = new TcpListener(IPAddress.Parse(textBox1.Text), Int32.Parse(textBox2.Text));
+                        /*Inicia o servidor TCP*/
+                        servidorTCP.Start();
+                        /*Escrever na caixa de LOG que o servidor está conectado*/
+                        BeginInvoke(new Delegate_void_String(Append_Text), new object[] { richTextBox1,
+                        string.Format("Servidor conectado. IP: {0} . Porta: {1} .", textBox1.Text, textBox2.Text)});
+                        /*Dá inicio a uma operação assíncrona que irá aguardar uma conexão
+                         de cliente. É necessário passar um callback que será executado quando
+                         ocorrer a conexão e o um objeto para ser manuseado, neste caso a variavel
+                         do servidor. */
+                        servidorTCP.BeginAcceptTcpClient(Callback_AceitarCliente, servidorTCP);
+                        /*Altera o texto do botão para quando ocorrer um novo clique, feche o
+                         servidor.*/
+                        button1.Text = "Fechar Servidor";
+                        break;
+
+                    case "Fechar Servidor":
+                        /*Para fechar o servidor sem causar erros, primeiro é necessário fechar a stream de dados.
+                          É uma boa prática testar se os elementos não são nulos antes de manipulalos
+                         */
+                        if(streamTCP != null)
+                        {
+                            streamTCP.Close();
+                        }
+                        /* Agora é possível fechar o servidor sem que aconteçam maiores problemas */
+                        if(servidorTCP != null)
+                        {
+                            servidorTCP.Stop();
+                        }
+                        /* Voltamos o botão para a função de abrir servidor */
+                        button1.Text = "Abrir Servidor";
+                        break;
+                }
+            }
+            /* Aqui realiza-se o tratamento de erros, neste caso apenas iremos fechar a stream de dados
+             * e o servidor em caso de erro, e entao retornar o botão para a função de abrir servidor */
+            catch(Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show("Houve um erro no servidor TCP. O servidor foi fechado.", "Atenção");
+                if (streamTCP != null)
+                {
+                    streamTCP.Close();
+                }
+                if (servidorTCP != null)
+                {
+                    servidorTCP.Stop();
+                }
+                button1.Text = "Abrir Servidor";
+            }
+        }
+        public delegate void Delegate_void_String(RichTextBox textbox, string text);
+        public void Append_Text(RichTextBox textbox, string text)
+        {
+            textbox.AppendText(text+"\r\n");
+            textbox.ScrollToCaret();
+        }
+        private void Callback_AceitarCliente(IAsyncResult ar)
+        {
+            servidorTCP = ar.AsyncState as TcpListener;
+            //servidorTCP = listener;
+            if (servidorTCP == null)
+                return;
+
+            try
+            {
+                clienteTCP = servidorTCP.EndAcceptTcpClient(ar);
+                streamTCP = clienteTCP.GetStream();
+                byte[] buffer = new byte[100];
+                streamTCP.BeginRead(buffer, 0, buffer.Length, Callback_LerCliente, clienteTCP);
+                BeginInvoke(new Delegate_void_String(Append_Text), new object[] { richTextBox1,
+                        string.Format("Cliente conectado.")});
+            }
+            finally
+            {
+                //servidorTCP.BeginAcceptTcpClient(Callback_AceitarCliente, servidorTCP);
+            }
+        }
+
+        private void Callback_LerCliente(IAsyncResult ar)
+        {
+            clienteTCP = ar.AsyncState as TcpClient;
+            byte[] bytes = new byte[100];
+            int i;
+            if(clienteTCP.Connected)
+            {
+                streamTCP = clienteTCP.GetStream();
+            }
+            // Loop to receive all the data sent by the client.
+
+            while (clienteTCP.Connected && streamTCP.CanRead)
+            {
+                if (streamTCP.DataAvailable)
+                {
+                    i = streamTCP.Read(bytes, 0, bytes.Length);
+                    BeginInvoke(new Delegate_void_String(Append_Text), new object[] { richTextBox1,
+                        string.Format("Mensagem recebida: {0}", Encoding.UTF8.GetString(bytes, 0, i))});
+                }
+            }
+
+            BeginInvoke(new Delegate_void_String(Append_Text), new object[] { richTextBox1,
+                        "O cliente se desconectou" });
+            // Shutdown and end connection
+            //clienteTCP.Close();
+        }
+    }
+}
